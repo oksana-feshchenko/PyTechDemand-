26c4f7a4b35f9a36e65a7deb4c6c3281de8864d5
oksana-feshchenko <foksanv@gmail.com>
2023-05-15 21:07:48 +0300

    add parse.py and analysys.ipynb

--- parse.py
+++ parse.py
@@ -1,13 +1,13 @@
+import asyncio
+import aiohttp
 import csv
-import logging
-import sys
 from dataclasses import dataclass, fields, astuple
 
 
-import requests
 from bs4 import BeautifulSoup
 
 URL = "https://djinni.co/jobs/?primary_keyword=Python"
+
 CSV_PATH = "description.csv"
 
 
@@ -19,45 +19,24 @@ class Job:
 
 JOB_FIELDS = [field.name for field in fields(Job)]
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(levelname)8s] : %(message)s",
-    handlers=[
-        logging.FileHandler("parser.log"),
-        logging.StreamHandler(sys.stdout),
-    ],
-)
 
-
-def get_num_pages(page_soup: BeautifulSoup) -> int:
+async def get_num_pages(page_soup: BeautifulSoup) -> int:
     pagination = page_soup.select_one(".pagination")
     if pagination is None:
         return 1
-    return pagination.select("li a")[-2].text
+    return int(pagination.select("li a")[-2].text)
 
 
-def parse_one_job(job_soup):
+async def parse_one_job(job_soup):
     return Job(
         title=job_soup.select_one('div.list-jobs__title a span').text,
         description=job_soup.select_one(".text-card").text.strip().replace("●", ""),
     )
 
 
-def get_one_page_jobs(page_soup):
+async def get_one_page_jobs(page_soup):
     jobs = page_soup.select(".list-jobs__item, .list__item")
-    return [parse_one_job(job) for job in jobs]
-
-
-def get_job_descriptions():
-    page = requests.get(URL).content
-    first_page_soup = BeautifulSoup(page, "html.parser")
-    num_pages = int(get_num_pages(first_page_soup))
-    all_jobs = get_one_page_jobs(first_page_soup)
-    for i in range(2, num_pages + 1):
-        page = requests.get(URL, {"page": i}).content
-        page_soup = BeautifulSoup(page, "html.parser")
-        all_jobs.extend(get_one_page_jobs(page_soup))
-    return all_jobs
+    return await asyncio.gather(*[parse_one_job(job) for job in jobs])
 
 
 def write_products_to_scv(jobs: [Job]) -> None:
@@ -67,8 +46,33 @@ def write_products_to_scv(jobs: [Job]) -> None:
         writer.writerows([astuple(job) for job in jobs])
 
 
-if __name__ == '__main__':
-    jobs = get_job_descriptions()
-    write_products_to_scv(jobs)
+async def get_job_descriptions(years_of_experience: int = None) -> None:
+    async with aiohttp.ClientSession() as session:
+        if years_of_experience:
+            params = {"exp_level": str(years_of_experience) + "y"}
+            page = await session.get(URL, params=params)
+        page = await session.get(URL)
+        content = await page.text()
+        first_page_soup = BeautifulSoup(content, "html.parser")
+        num_pages = await get_num_pages(first_page_soup)
+        all_jobs = await get_one_page_jobs(first_page_soup)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        tasks = []
+        for i in range(2, num_pages + 1):
+            params = {"page": i}
+            page = await session.get(URL, params=params)
+            content = await page.text()
+            page_soup = BeautifulSoup(content, "html.parser")
+            tasks.append(get_one_page_jobs(page_soup))
+
+        results = await asyncio.gather(*tasks)
+        for jobs in results:
+            all_jobs.extend(jobs)
+
+        write_products_to_scv(all_jobs)
+
+
+if __name__ == '__main__':
+    asyncio.run(get_job_descriptions())
+
+
